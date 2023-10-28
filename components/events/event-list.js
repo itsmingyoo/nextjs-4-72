8104cf119c450973f5dd8f301033be510f821add
Minh Tran <minhttran93@gmail.com>
2023-10-28 06:45:53 -0700

    formatted dynamic info

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,9 +1,11 @@
+import EventItem from "./event-item";
+
 function EventList(props) {
   const { items } = props;
   return (
     <ul>
       {items.map((event) => (
-        <li></li>
+        <EventItem />
       ))}
     </ul>
   );
