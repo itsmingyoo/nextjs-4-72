619859445797704bf1d9783ac8be6f0b911e89af
Minh Tran <minhttran93@gmail.com>
2023-10-28 11:51:46 -0700

    use event details components in the eventId.js route

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,9 @@
+import { Fragment } from "react";
 import { useRouter } from "next/router";
 import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 function EventDetailPage() {
   const router = useRouter();
@@ -9,9 +13,18 @@ function EventDetailPage() {
   if (!event) return <p>No event Found!</p>;
 
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 export default EventDetailPage;
