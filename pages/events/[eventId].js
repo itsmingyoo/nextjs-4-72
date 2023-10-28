59f0de094bd2c5731e964c1607510abcf9aedb3e
Minh Tran <minhttran93@gmail.com>
2023-10-28 11:41:09 -0700

    add event grabber, add event-details from lecture

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,13 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+
 function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId; // here we are accessing the key 'eventId' from the query and getting its value which is the variable we create here
+  const event = getEventById(eventId); // pass in eventId to get the event
+
+  if (!event) return <p>No event Found!</p>;
+
   return (
     <div>
       <h1>Event Detail</h1>
